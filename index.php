d789d88cd102da7adc4533f9c41be1b25d79a815
samshal <samueladeshina73@gmail.com>
2021-01-13 19:50:25 +0100

    Setup of API Middleware, authorization and login endpoint

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use RFHApi\Middleware\PermissionGateway as PermissionGatewayMiddleware;
+
+$settings = [
+	'settings'=>[
+		'determineRouteBeforeAppMiddleware' => true,
+		'displayErrorDetails' => true
+	]
+];
+
+header('Access-Control-Allow-Headers: Content-Type, Cache-Control, X-Requested-With, Authorization');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
+$permissionGatewayMiddleware = new PermissionGatewayMiddleware();
+$app = new \Slim\App($settings);
+
+$app->post('/{version}/login', function($request, $response, $args){
+	$data = $request->getParsedBody();
+
+	$pluginResponse = []; //determine from login controller
+
+	if (isset($pluginResponse["status"]) && $pluginResponse["status"]){
+
+		return $response->withJson(["errorStatus"=>false, "errorMessage"=>null, "contentData"=>$pluginResponse], 200);
+	}
+
+	return $response->withJson(["errorStatus"=>false, "errorMessage"=>null, "contentData"=>["status"=>false, "reason"=>"Invalid Username or Password"]], 200);
+});
+
+$app->group('/', function(){
+	$this->map(
+		['GET', 'POST', 'PUT', 'DELETE'],
+		'{version}/{module}/{resource}/{action}[/{resourceId}]',
+		function($request, $response, $args){
+			$globalResponseFormat = [
+				"body"=>[
+					"errorStatus"=>false,
+					"errorMessage"=>NULL,
+					"contentData"=>[]
+				],
+				"status"=>200
+			];
+
+			if ($request->isGet() || $request->isDelete())
+			{
+				$parsedBody = $request->getQueryParams();
+			}
+			else
+			{
+				$parsedBody = $request->getParsedBody();
+			}
+			
+			if (!is_array($parsedBody)){
+				$globalResponse = [
+					"body"=>[
+						"errorStatus"=>true,
+						"errorType"=>"Error",
+						"errorMessage"=>"No data detected."
+					],
+					"status"=>400
+				];
+
+				return $response->withJson($globalResponse["body"], $globalResponse["status"]);
+			}
+
+			$options = array_merge($args, $parsedBody);
+			
+			$globalResponse = array_replace_recursive(
+				$globalResponseFormat, 
+				EmmetBlueMiddleware\Middleware::processor($options, "")
+			);
+
+			return $response->withJson($globalResponse["body"], $globalResponse["status"]);
+	})->add(EmmetBlueMiddleware\Middleware::validateRequest());
+})->add($permissionGatewayMiddleware->getStandardResponse());
+
+$app->run();
---
+++ src/Middleware/PermissionGateway.php
@@ -0,0 +1,127 @@
+<?php declare(strict_types=1);
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+use EmmetBlue\Core\Constant;
+
+namespace RFHApi\Middleware;
+
+class PermissionGateway implements \EmmetBlueMiddleware\MiddlewareInterface
+{
+	protected static function isPublicEndpoint($endpoint){
+		$endpoints = [];
+
+		try {
+			$endpointsFile = Constant::getGlobals()["config-dir"]["public-endpoints"];
+			if (file_exists($endpointsFile)){
+				$endpoints = json_decode(file_get_contents($endpointsFile));
+			}
+		}
+		catch (\TypeError $e) {
+		}
+
+		$endpoint = implode("__", $endpoint);
+
+		return in_array($endpoint, $endpoints);
+	}
+
+	protected static function isUserLoggedIn($userId, $sessionId, $userToken)
+	{
+		$isTokenValid = \KuboPlugin\User\UserSession\Session::isTokenValid($userId, ["sessionId"=>$sessionId, "token"=>$userToken]);
+
+		return $isTokenValid["status"];
+	}
+
+	protected static function isUserPermitted($userId, $sessionId, $endpoint)
+	{
+		$session = \KuboPlugin\User\UserSession\Session::load((int)$userId, (int)$sessionId);
+		$accountType = $session[0]["AccountTypeName"];
+
+		if (is_null($accountType)){
+			return false;
+		}
+
+		$requestData = [
+			"accountType"=>$accountType,
+			"endpoint"=>$endpoint
+		];
+
+		$isUserPermitted = \KuboPlugin\User\UserPermission\Acl::checkPermission($userId, $requestData);
+
+		return $isUserPermitted["status"];
+	}
+
+	public function getStandardResponse()
+	{
+		return function(RequestInterface $request, ResponseInterface $response, callable $next)
+		{
+			$args = $request->getAttribute('routeInfo')[2];
+
+			if (!isset($args["resourceId"])){
+				$qs = explode("&",$request->getServerParams()["QUERY_STRING"]);
+				foreach($qs as $string){
+					$explode = explode("=",$string);
+					if ("resourceId" == $explode[0]){
+						$args["resourceId"] = $explode[1];
+						break;
+					}
+					continue;
+				}
+			}
+
+			$module = $args['module'];
+			$resource = $args['resource'];
+			$action = $args['action'];
+
+			$token_string = (isset($request->getHeaders()["HTTP_AUTHORIZATION"][0])) ? $request->getHeaders()["HTTP_AUTHORIZATION"][0] : "";
+
+			$token = explode(",", $token_string); //token_string format: token,session_id,user_id
+
+			$sessionId = $token[1] ?? 0;
+			$userId = (int)$token[2] ?? 0;
+			$token = $token[0] ?? 0;
+			
+			$endpoint = [$module, $resource, $action];
+
+			if (!self::isPublicEndpoint($endpoint)){
+				if (!self::isUserLoggedIn($userId, $sessionId, $token))
+				{
+					$globalResponse = [];
+
+					$globalResponse["status"] = 401;
+					$globalResponse["body"]["errorStatus"] = true;
+					$globalResponse["body"]["errorMessage"] = "You havent been logged in or your supplied login token is invalid.";
+
+					return $response->withJson($globalResponse["body"], $globalResponse["status"]);
+				}
+
+				try
+				{
+					if (!self::isUserPermitted($userId, $sessionId, $endpoint) || (isset($args["resourceId"]) && $userId != $args["resourceId"]))
+					{
+						$globalResponse = [];
+
+						$globalResponse["status"] = 401;
+						$globalResponse["body"]["errorStatus"] = true;
+						$globalResponse["body"]["errorMessage"] = "You do not have the appropriate permissions to perform the requested operation";
+
+						return $response->withJson($globalResponse["body"], $globalResponse["status"]);
+					}
+				}
+				catch(\Exception $e)
+				{
+					$globalResponse = [];
+
+					$globalResponse["status"] = 403;
+					$globalResponse["body"]["errorStatus"] = true;
+					$globalResponse["body"]["errorMessage"] = "Authentication failed, please resend a valid request. ".$e->getMessage();
+
+					return $response->withJson($globalResponse["body"], $globalResponse["status"]);
+				}
+			}
+
+			return $next($request, $response);
+		};
+	}
+}
