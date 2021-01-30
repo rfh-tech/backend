ac5dc00410ee808634bce0dd2376e48c7cf2c8f5
samshal <samueladeshina73@gmail.com>
2021-01-30 14:19:57 +0100

    added crops json

---
+++ src/PublicDataset/Controllers/Crops.php
@@ -0,0 +1,34 @@
+<?php declare (strict_types=1);
+/**
+ * Controller Class.
+ *
+ * This file is part of RFHubAPI, please read the documentation
+ * available in the root level of this project
+ *
+ * @license MIT
+ * @author Samuel Adeshina <samueladeshina73@gmail.com>
+ *
+ */
+
+
+namespace RFHApi\PublicDataset\Controllers;
+
+use EmmetBlue\Core\Connection\ConnectionAdapter;
+
+
+/**
+ * class RFHApi\PublicDataset\Controllers\Crops
+ *
+ * Crops Controller
+ *
+ * @author Samuel Adeshina <samueladeshina73@gmail.com>
+ * @since v0.0.1 30/01/2021 10:13
+ */
+class Crops {
+    public function getCrops(){
+        $cropsJson = file_get_contents(dirname(__FILE__)."\bin\crops.json");
+        $crops = json_decode($cropsJson);
+
+        return $crops;
+    }
+}
---
+++ src/PublicDataset/Crops.php
@@ -0,0 +1,29 @@
+<?php declare (strict_types=1);
+/**
+ * Visitor Class.
+ *
+ * This file is part of RFHubAPI, please read the documentation
+ * available in the root level of this project
+ *
+ * @license MIT
+ * @author Samuel Adeshina <samueladeshina73@gmail.com>
+ *
+ */
+
+namespace RFHApi\PublicDataset;
+
+/**
+ * class RFHApi\PublicDataset\Crops
+ *
+ * Crops Visitor
+ * 
+ * This class returns list of countries, states, cities in the Crops
+ *
+ * @author Samuel Adeshina <samueladeshina73@gmail.com>
+ * @since v0.0.1 30/01/2021 10:09
+ */
+class Crops {
+    public function getCrops(){
+        return Controllers\Crops::getCrops();
+    }
+}
